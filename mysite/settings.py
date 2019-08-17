555e58b75426734f06b7eea064297a766b02f790
Asim Okby <asemokbey@gmail.com>
2019-08-17 21:28:54 +0300

    updated staticroot

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,3 +158,19 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
