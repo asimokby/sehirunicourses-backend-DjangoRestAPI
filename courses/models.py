c0e0147d46669a65f40dfad575c47fc0fd4bdac9
Asim Okby <asemokbey@gmail.com>
2019-08-23 02:48:56 +0300

    nothing important

--- courses/models.py
+++ courses/models.py
@@ -6,7 +6,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=20)
     teacher = models.CharField(max_length=20)
-    syllabus = models.FileField(upload_to='syllabus', blank=True)
+    syllabus = models.FileField(upload_to='syllabus')
 
 
     def __str__(self):
